1c12f9e492d1df13df92f60872ee923cb702ca0e
Keene Chin <trix1011@gmail.com>
2018-02-04 22:48:53 -0600

    expanded node

--- BasicSearch/src/GenericSearch.java
+++ BasicSearch/src/GenericSearch.java
@@ -11,52 +11,55 @@ import java.util.Scanner;
 public class GenericSearch {
 
 	public static String state;
+	private static Scanner fileScan;
 	public static void main(String[] args) throws FileNotFoundException {
 		
 
 		File testFile = new File("./src/tile1.txt");
-		Scanner fileScan = new Scanner(testFile);
+		fileScan = new Scanner(testFile);
 		state = fileScan.next();
 		Queue<Node> fringe = null;
 		String strategy= "BFS";
 		if(strategy.equals("BFS")){
 			 fringe= new LinkedList<Node>();
 		}
+		ArrayList<String> visited= new ArrayList<String>();
 		
 		Node root = new Node(state);
 		fringe.add(root);
 		
 		while(!fringe.isEmpty()) {
 			Node n = fringe.poll();
+			System.out.println(n.toString());
+		
+			//System.out.println(visited);
 			if(isGoal(n)) {
-				System.out.println(path(n));
+				System.out.println(path(n,0));
 				break;
 			}
-			else {
+			else if(!visited.contains(n.state)){			
+		 
 				List<Node> s = successors(n);
-				fringe.addAll(s);
+				fringe.addAll(s);	
+				visited.add(n.state);
 			}
+			
 		}
 		
-		
-		
-		
-		//System.out.println(successors(root).size());
 	}
 	
-	public static String path(Node n) {
+	public static String path(Node n, int step) {
+		
 		String path="";
-		int step = 0;
 		if(n.parent!=null) {
 			step++;
-			path=path.concat(path(n.parent)+"Step "+step+": "+"move "+n.move+" "+n.state+"\n");
+			path=path.concat(path(n.parent,step)+"Step "+step+": "+n.toString()+"\n");
 
 		}
 		
 		else {
-			return "Step 0: "+n.state+'\n';
+			return "Step 0: "+n.toString()+'\n';
 		}
-			
 		return path;
 	}
 	
@@ -64,13 +67,19 @@ public class GenericSearch {
 	public static List<Node> successors(Node n){
 		List<Node> succs = new ArrayList<Node>();
 		String st=n.state;
+		int blankIndex = 0;
 		char[] stateChars = st.toCharArray();
 		for(int i=0; i<stateChars.length; i++) {
-			if(stateChars[i]!='x') {
-				Node m = new Node(move(i,st));
-				m.move=i;
-				m.parent=n;
-				succs.add(m);
+			if(stateChars[i]=='x') {
+				blankIndex=i;
+			}
+		}
+		for(int i=0; i<stateChars.length; i++) {
+			if(i!=blankIndex){		
+					Node m = new Node(move(i,st));
+					m.move=i;
+					m.parent=n;
+					succs.add(m);	
 			}
 		}
 		return succs;
--- BasicSearch/src/Node.java
+++ BasicSearch/src/Node.java
@@ -10,12 +10,17 @@ public class Node{
 	
 	public Node(String st) {
 		state = st;
-		move = -1;
+		move=-1;
 	}
 
 	public void addChildren() {
 		
 	}
 
-	
+	public String toString() {
+		if(move>=0) {
+			return "Move "+move+" "+state;
+		}
+		else return state;
+	}
 }
