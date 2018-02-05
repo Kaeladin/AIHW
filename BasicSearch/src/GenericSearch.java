3ff9e80b1e0f6af5b84c8552343b6cba6fa01078
Keene Chin <trix1011@gmail.com>
2018-02-04 21:56:31 -0600

    BFS

--- BasicSearch/src/GenericSearch.java
+++ BasicSearch/src/GenericSearch.java
@@ -1,21 +1,65 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class GenericSearch {
 
 	public static String state;
 	public static void main(String[] args) throws FileNotFoundException {
+		
 
 		File testFile = new File("./src/tile1.txt");
 		Scanner fileScan = new Scanner(testFile);
 		state = fileScan.next();
-
+		Queue<Node> fringe = null;
+		String strategy= "BFS";
+		if(strategy.equals("BFS")){
+			 fringe= new LinkedList<Node>();
+		}
+		
 		Node root = new Node(state);
-		System.out.println(successors(root).size());
+		fringe.add(root);
+		
+		while(!fringe.isEmpty()) {
+			Node n = fringe.poll();
+			if(isGoal(n)) {
+				System.out.println(path(n));
+				break;
+			}
+			else {
+				List<Node> s = successors(n);
+				fringe.addAll(s);
+			}
+		}
+		
+		
+		
+		
+		//System.out.println(successors(root).size());
 	}
+	
+	public static String path(Node n) {
+		String path="";
+		int step = 0;
+		if(n.parent!=null) {
+			step++;
+			path=path.concat(path(n.parent)+"Step "+step+": "+"move "+n.move+" "+n.state+"\n");
+
+		}
+		
+		else {
+			return "Step 0: "+n.state+'\n';
+		}
+			
+		return path;
+	}
+	
 	
 	public static List<Node> successors(Node n){
 		List<Node> succs = new ArrayList<Node>();
@@ -23,7 +67,10 @@ public class GenericSearch {
 		char[] stateChars = st.toCharArray();
 		for(int i=0; i<stateChars.length; i++) {
 			if(stateChars[i]!='x') {
-				succs.add(new Node(move(i,st)));
+				Node m = new Node(move(i,st));
+				m.move=i;
+				m.parent=n;
+				succs.add(m);
 			}
 		}
 		return succs;
--- BasicSearch/src/Node.java
+++ BasicSearch/src/Node.java
@@ -1,18 +1,21 @@
 import java.util.List;
 
-public class Node {
+public class Node{
 	public String state;
 	public List<Node> children; 
 	public Node parent;
 	public int g;
 	public int h;
+	public int move;
 	
 	public Node(String st) {
 		state = st;
+		move = -1;
 	}
 
 	public void addChildren() {
 		
 	}
 
+	
 }
