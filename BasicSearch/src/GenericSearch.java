b05c82f8e7fcfcf9ae38e6230cd9388c8fded3a7
Keene Chin <trix1011@gmail.com>
2018-02-04 20:49:23 -0600

    successor, operation, goal

--- BasicSearch/src/GenericSearch.java
+++ BasicSearch/src/GenericSearch.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GenericSearch {
@@ -11,13 +13,50 @@ public class GenericSearch {
 		Scanner fileScan = new Scanner(testFile);
 		state = fileScan.next();
 
-		
-		
+		Node root = new Node(state);
+		System.out.println(successors(root).size());
 	}
 	
+	public static List<Node> successors(Node n){
+		List<Node> succs = new ArrayList<Node>();
+		String st=n.state;
+		char[] stateChars = st.toCharArray();
+		for(int i=0; i<stateChars.length; i++) {
+			if(stateChars[i]!='x') {
+				succs.add(new Node(move(i,st)));
+			}
+		}
+		return succs;
+	}
 	
-	public static void move(int x) {
+	public static boolean isGoal(Node n)
+	{		
+		boolean expectWhite = false;
+		char[] stateChars =  n.state.toCharArray();
+		for(int i=0; i<stateChars.length; i++) {
+			if(expectWhite && stateChars[i]=='B')
+				return false;
+			if(!expectWhite && stateChars[i]=='W')
+				return false;
+			if(stateChars[i]=='x')
+				expectWhite=true;
+		}		
+		return true;
+	}//Goal test
+	
+	public static String move(int x, String state) {
+		int blankIndex = 0;
 		
+		char[] stateChars = state.toCharArray();
+		for(int i=0; i<stateChars.length; i++) {
+			if(stateChars[i]=='x') {
+				blankIndex=i;
+			}
+		}
+		
+		stateChars[blankIndex]=stateChars[x];
+		stateChars[x]='x';
+		return new String(stateChars);
 	}
 
 }
--- BasicSearch/src/Node.java
+++ BasicSearch/src/Node.java
@@ -4,6 +4,15 @@ public class Node {
 	public String state;
 	public List<Node> children; 
 	public Node parent;
+	public int g;
+	public int h;
 	
+	public Node(String st) {
+		state = st;
+	}
+
+	public void addChildren() {
+		
+	}
 
 }
